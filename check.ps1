e179f2cc4a895dd73fdc33873427015b1dd8dd97
Warren Sherwen <warren.sherwen@aspirets.com>
2024-10-17 09:45:10 +0100

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,4 +1,4 @@
-if (test-path "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads\TeamsBackground.png") {
+if (test-path "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads\66053900-762a-4251-869b-44288c4a276f.png") {
 Write-Host "Teams Background Found"
 exit 0
 }
